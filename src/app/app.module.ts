6519cc2fc4890d432e733d12b9f7204e2ae620a9
Atzuke <c.riman@hotmail.com>
2018-06-12 09:23:42 -0500

    Creacion del modulo de routing y el componente de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import { AppComponent } from './app.component';
 import { ProjectListComponent } from './auth/project-list/project-list.component';
 import { HeaderComponent } from './common/header/header.component';
 import { LoaderComponent } from './common/loader/loader.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectListComponent,
     HeaderComponent,
-    LoaderComponent
+    LoaderComponent,
+    UserComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule
